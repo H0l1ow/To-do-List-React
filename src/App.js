b55a0c11d49a2b1f561a966e2b35ea364df6f151
Damian Skórka <unearth179@gmail.com>
2023-11-13 18:43:53 +0100

    added delete and edit function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+// Task function for handling a single task in a list
+function Task({ task, index, completeTask, deleteTask, editTask }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <li style={{ textDecoration: task.completed ? 'line-through' : '' }}>
+      {task.name}
+      <div>
+        <button onClick={() => completeTask(index)}>Done</button>
+        <button onClick={() => deleteTask(index)}>Delete</button>
+        <button onClick={() => editTask(index, task.name)}>Edit</button>
+      </div>
+    </li>
+  );
+}
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState('');
+  const [editing, setEditing] = useState(false);
+  const [editIndex, setEditIndex] = useState(null);
+
+  useEffect(() => {
+    // Restoring data from local storage
+    const storedTasks = JSON.parse(localStorage.getItem('tasks'));
+    if (storedTasks) {
+      setTasks(storedTasks);
+    }
+  }, []);
+
+  useEffect(() => {
+    // Save data for local storage
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
+  const addTask = () => {
+    if (task.trim() !== '') {
+      if (editing) {
+        // Edit task
+        const newTasks = [...tasks];
+        newTasks[editIndex].name = task;
+        setTasks(newTasks);
+        setEditing(false);
+        setEditIndex(null);
+      } else {
+        // Add task
+        setTasks([...tasks, { name: task, completed: false }]);
+      }
+      setTask('');
+    }
+  };
+
+  const completeTask = (index) => {
+    const newTasks = [...tasks];
+    newTasks[index].completed = !newTasks[index].completed;
+    setTasks(newTasks);
+  };
+
+  const deleteTask = (index) => {
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  };
+
+  const editTask = (index, name) => {
+    setTask(name);
+    setEditing(true);
+    setEditIndex(index);
+  };
+
+  // Structure
+  return (
+    <div className="container">
+      <h1>To-do List</h1>
+      <div className="add-task-tab">
+        <input
+          type="text"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+          placeholder="Add task..."
+        />
+        <button className="add-btn" onClick={addTask}>{editing ? 'Edit' : 'Add'}</button>
+      </div>
+      <ul>
+        {tasks.map((task, index) => (
+          <Task
+            key={index}
+            index={index}
+            task={task}
+            completeTask={completeTask}
+            deleteTask={deleteTask}
+            editTask={editTask}
+          />
+        ))}
+      </ul>
     </div>
   );
 }
